9391371c733bef9fab1abb68c0cdd09b0e2eacbf
Suyono Sukorame <suyonosukorame@ymail.com>
2025-08-14 07:20:35 +0800

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by admin on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteAppTests/NoteAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  NoteAppTests.swift
+//  NoteAppTests
+//
+//  Created by admin on 14/08/25.
+//
+
+import Testing
+
+struct NoteAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
